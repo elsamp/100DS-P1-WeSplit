fcbfa52b212bbea26b7898827dc9234d4869dd2d
Erica Sampson <ericabcherry@gmail.com>
2024-07-10 17:24:46 -0400

    Initial Commit

---
+++ 100DS-P1-WeSplit/_00DS_P1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P1_WeSplitApp.swift
+//  100DS-P1-WeSplit
+//
+//  Created by Erica Sampson on 2024-07-10.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
